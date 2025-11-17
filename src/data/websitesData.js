44c2c8bd961eeb627c1e565e8082b1dc4d276954
edogev <83846346+edogev@users.noreply.github.com>
2025-11-17 19:34:35 +0300

    add photo site

--- src/data/websitesData.js
+++ src/data/websitesData.js
@@ -4,6 +4,6 @@ export const websitesData = [
     id: 1,
     title: 'Лендинг: Риелтор',
     description: 'Лендинг риелтора с подробной информацией об услугах, портфолио объектов, отзывами и удобной формой связи.',
-    image: 'https://via.placeholder.com/1200x800.png?text=E-commerce+Fashion+Store'
+    image: 'https://raw.githubusercontent.com/edogev/site/refs/heads/main/src/img/mercher/site_reeltor.png'
   },
 ];
